e20087781c49bd3c5e7330556a9b4626bc844470
Omadbek <omadbekolimov96@gmail.com>
2024-10-05 18:01:25 +0500

    gooo

--- src/pages/Server.tsx
+++ src/pages/Server.tsx
@@ -1,23 +1,34 @@
-import {Cards} from "../components";
+import { Cards } from "../components";
 import Heading from "../components/heading";
+
+// Import images from the assets folder
+import qrApp from "../assets/qr-section17.avif";
+
 
 const Server = () => {
     return (
         <div className="max-container">
-            <Heading text={`Markaziy server`}/>
+            <Heading text="Markaziy server" />
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 <Cards
+                    imgSrc={""}
                     title="API va Backend mantig'i"
                     description="Barcha funksiyalar API orqali amalga oshiriladi."
                 />
                 <Cards
+                    imgSrc={''}
                     title="Ma'lumotlar bazasi"
                     description="Tizim foydalanuvchilar va tranzaksiyalar haqida ma'lumotlarni saqlaydi."
                 />
                 <Cards
+                    imgSrc={''}
                     title="Xizmat ko'rsatish"
                     description="Foydalanuvchilar uchun barcha xizmatlar markaziy server orqali taqdim etiladi."
                 />
+            </div>
+            <div>
+                <Heading text="Markaziy server" />
+                <img src={qrApp} width={450} />
             </div>
         </div>
     );
