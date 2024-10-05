f17bf98603fe26807a63106a2cf6b3bf5a64c7a8
Alisher <alishersodiqov09@gmail.com>
2024-10-05 16:58:37 +0500

    rang full

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
     theme: {
         extend: {
-            colors: {}
+            colors: {
+                darkBlue: '#333F70',
+                green: '#26A688',
+                lightGreen: '#D6F5EE',
+                bgDarkBlue: '#192248',
+                dark: '#000',
+                white: '#fff',
+            }
         },
     },
     plugins: [],
