86c688c8cbb9f16270250a8a13b997bf897db678
Omadbek <omadbekolimov96@gmail.com>
2024-10-05 17:38:49 +0500

    gooo

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -8,9 +8,9 @@ interface MobileAppCardProps {
 
 const Cards: React.FC<MobileAppCardProps> = ({ imgSrc, title, description }) => {
   return (
-    <div className="flex flex-col items-center text-center space-y-3">
+    <div className="w-full flex flex-row justify-between space-x-4">
       {imgSrc && <img src={imgSrc} alt="Mobile App Icon" className="w-16 h-16" />}
-      <h2 className="text-xl font-bold text-lightGreen">{title}</h2>
+      <h2 className="text-2xl font-bold text-lightGreen">{title}</h2>
       <p className="text-lightGreen">{description}</p>
     </div>
   );
