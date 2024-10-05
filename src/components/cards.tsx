65bdfc5f3b22fcf9804bc63e81d4ef5e8c1f0ed1
Omadbek <omadbekolimov96@gmail.com>
2024-10-05 17:14:06 +0500

    cards

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,9 +1,19 @@
-const Cards = () => {
-    return (
-        <div>
+import React from 'react';
 
-        </div>
-    );
+interface MobileAppCardProps {
+  imgSrc?: string;
+  title: string;
+  description: string;
+}
+
+const Cards: React.FC<MobileAppCardProps> = ({ imgSrc, title, description }) => {
+  return (
+    <div className="flex flex-col items-center text-center space-y-3">
+      {imgSrc && <img src={imgSrc} alt="Mobile App Icon" className="w-16 h-16" />}
+      <h2 className="text-xl font-bold text-lightGreen">{title}</h2>
+      <p className="text-lightGreen">{description}</p>
+    </div>
+  );
 };
 
 export default Cards;
