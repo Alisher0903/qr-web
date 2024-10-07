6c66bbe36a528af5b542d30564b9ff7c25940d61
Alisher <alishersodiqov09@gmail.com>
2024-10-07 12:43:54 +0500

    qr code xam buldi

--- src/pages/alisher/qr-create.tsx
+++ src/pages/alisher/qr-create.tsx
@@ -2,14 +2,36 @@ import React from "react";
 import Heading from "../../components/heading.tsx";
 import {Cards} from "../../components";
 
+const data = [
+    {
+        id: 1,
+        name: "To'lov miqdorini kiritish",
+        des: 'Savdogar to\'lov miqdorini kiritadi yoki xaridor uni dinamik ravishda kiritish imkoniyatiga ega bo\'ladi.'
+    },
+    {id: 2, name: "QR-kodni yaratish", des: "Tizim QR-kodni yaratadi va uni xaridorga taqdim etadi."},
+    {id: 3, name: "Tavsif qo'shish", des: "To'lovga ixtiyoriy ravishda tavsif qo'shish mumkin."},
+]
+
 const QrCreate: React.FC = () => {
     return (
         <div className={'flex justify-center items-center bg-bgDarkBlue w-full min-h-screen'}>
-            <div className="w-[60%] h-[90%] bg-white p-16 my-10 rounded-xl">
+            <div className="w-[95%] lg:w-[60%] h-[90%] bg-white p-16 my-10 rounded-xl">
                 <Heading text={'QR-kod yaratish'}/>
                 <div className="flex flex-col md:flex-row justify-center items-start md:items-center gap-16 mt-10">
+                    <div className={`lg:hidden grid grid-cols-1 md:grid-cols-2 gap-6`}>
+                        {data.map(item => (
+                            <div className={'flex justify-start items-start gap-6'} key={item.id}>
+                                <StepNumber number={item.id}/>
+                                <Cards
+                                    title={item.name}
+                                    description={item.des}
+                                />
+                            </div>
+                        ))}
+                    </div>
+
                     {/* Left Column */}
-                    <div className="flex flex-col items-start gap-12">
+                    <div className="lg:flex flex-col items-start gap-12 hidden">
                         <Cards
                             title={'To\'lov miqdorini kiritish'}
                             description={'Savdogar to\'lov miqdorini kiritadi yoki xaridor uni dinamik ravishda kiritish imkoniyatiga ega bo\'ladi.'}
@@ -21,14 +43,14 @@ const QrCreate: React.FC = () => {
                     </div>
 
                     {/* Middle Line with Numbers */}
-                    <div className="flex flex-col justify-between items-center gap-12">
+                    <div className="lg:flex flex-col justify-between items-center hidden gap-12">
                         <StepNumber number={1}/>
                         <StepNumber number={2}/>
                         <StepNumber number={3}/>
                     </div>
 
                     {/* Right Column */}
-                    <div className="flex flex-col items-start">
+                    <div className="lg:flex flex-col items-start hidden">
                         <Cards
                             title="Tavsif qo'shish"
                             description="To'lovga ixtiyoriy ravishda tavsif qo'shish mumkin."
