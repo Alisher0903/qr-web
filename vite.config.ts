73ea1c4ad2aa840da10d0ea68316a98cbfa1404f
Omadbek <omadbekolimov96@gmail.com>
2024-10-14 11:42:57 +0500

    port qo'shildi

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '185.74.4.138',  // IP manzil
+    port: 3000,            // Portni o'rnatish (kerakli portni kiriting)
+    strictPort: true       // Agar port band bo'lsa, xatolik qaytaradi
+  }
 })
